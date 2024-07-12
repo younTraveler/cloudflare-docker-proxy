75750af61be7032d98d09f434480096e0529ccef
younTraveler <65150657+younTraveler@users.noreply.github.com>
2024-07-12 20:36:16 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,13 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.mydomain.com": "https://registry-1.docker.io",
-  "quay.mydomain.com": "https://quay.io",
-  "gcr.mydomain.com": "https://gcr.io",
-  "k8s-gcr.mydomain.com": "https://k8s.gcr.io",
-  "k8s.mydomain.com": "https://registry.k8s.io",
-  "ghcr.mydomain.com": "https://ghcr.io",
-  "cloudsmith.mydomain.com": "https://docker.cloudsmith.io",
-
-  // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker.yountraveler.top": "https://registry-1.docker.io",
+  "quay.yountraveler.top": "https://quay.io",
+  "gcr.yountraveler.top": "https://gcr.io",
+  "k8s-gcr.yountraveler.top": "https://k8s.gcr.io",
+  "k8s.yountraveler.top": "https://registry.k8s.io",
+  "ghcr.yountraveler.top": "https://ghcr.io",
+  "cloudsmith.yountraveler.top": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
